e3a0ec8aff42891151fbd169ffdab6b2e1c09a41
Simona Šusterová <simsusterova@gmail.com>
2025-05-11 15:33:24 +0200

    hotovo

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+//1. tohle funguje
+//let displayElm = document.querySelector (".display")
+//displayElm.textContent = "42";  
 
+// řešení z kodím
+const displayElm = document.querySelector('.display');
+
+const handleDigitClick = (event) => {
+  if (displayElm.textContent.length === 9) {
+    return;
+  }
+  const digit = event.target.textContent;
+  if (displayElm.textContent === '0') {
+    displayElm.textContent = digit; // Na displeji je nula, chceme ji tedy nahradit zadanou číslicí.
+  } else {
+    displayElm.textContent += digit;
+  }
+};
